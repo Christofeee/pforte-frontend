b55a628e7fb530c1a8792a2f743b0087fb5034e8
Christopher Nicholas ( Zin Phyo Min) <chrizt0202@gmail.com>
2024-06-18 21:59:35 +0700

    implement module data

--- app/teacher/classroom/[id]/modules/components/moduleList.js
+++ app/teacher/classroom/[id]/modules/components/moduleList.js
@@ -4,16 +4,34 @@ import { Grid, Typography, Box, Button } from '@mui/material';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import getModules from '../utils/getModules';
 
 export default function ModuleList({ classId }) {
 
     const router = useRouter()
+    const [loading, setLoading] = useState(false)
+    const [modules, setModules] = useState([])
+    const [searchQuery, setSearchQuery] = useState('');
 
-    const [isInstructionComplete, setIsInstructionComplete] = useState(null)
-    console.log("set to: ", isInstructionComplete)
     useEffect(() => {
-        setIsInstructionComplete(true)
-    }, [])
+        const fetchData = async () => {
+            try {
+                setLoading(true); // Set loading state to true before fetching data
+
+                const modulesData = await getModules()
+                setModules(modulesData);
+
+                setLoading(false); // Set loading state to false after fetching data
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setLoading(false); // Set loading state to false in case of error
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    console.log(modules)
 
     const Checkbox = ({ isChecked, handleChange }) => {
         return (
@@ -34,41 +52,51 @@ export default function ModuleList({ classId }) {
         }
     }
 
-    const enterModule = ( classId , moduleId = 2) => {
+    const enterModule = (classId, moduleId = 2) => {
         router.push(`/teacher/classroom/${classId}/modules/${moduleId}`)
     }
 
+    const handleSearch = () => {
+        // Perform search logic here, filtering classes based on searchQuery
+        // For simplicity, let's assume we're filtering by class name
+        return modules.filter(module =>
+            module.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+    };
+
     return (
         <>
-            <Grid container spacing={2} className='p-1 pb-5 mb-5' alignItems={'center'}>
-                <Grid item xs={12} md={9}>
-                    <div className='bg-gray-100 p-5' style={{ borderRadius: '10px', display: 'flex' }}>
-                        <Box sx={{ flex: '1 1 auto', minWidth: 0 }}>
-                            <Typography variant='h6' noWrap>
-                                Full Module Name
+            {handleSearch().map((module) => (
+                <Grid container spacing={2} className='p-1 pb-5 mb-5' alignItems={'center'}>
+                    <Grid item xs={12} md={9}>
+                        <div className='bg-gray-100 p-5' style={{ borderRadius: '10px', display: 'flex' }}>
+                            <Box sx={{ flex: '1 1 auto', minWidth: 0 }}>
+                                <Typography variant='h6' noWrap>
+                                    {module.name}
+                                </Typography>
+                                <Typography variant='body2' color='textSecondary' noWrap className='p-2'>
+                                    {module.description} description that can be long and should stay in a single line and truncate if too lengthy. Phasellus sed sapien maximus, vestibulum urna ultricies, mattis metus. Etiam pretium cursus quam sit amet hendrerit. Morbi urna enim, fermentum ut vestibulum eget, ultricies ac eros. Sed suscipit porta massa, feugiat feugiat sem feugiat eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ex ex, eleifend
+                                </Typography>
+                            </Box>
+                            <Button
+                                onClick={() => enterModule(classId)}
+                                sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginLeft: '16px' }}>
+                                <ArrowForwardIosIcon />
+                            </Button>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                        <div className='' style={{ borderRadius: '10px' }}>
+                            <Typography variant='body2' color='textSecondary' className='flex'>
+                                <Checkbox isChecked={module.isComplete} handleChange={handleCheckboxChange} />
+                                <span className='px-5'>
+                                    Instruction Complete
+                                </span>
                             </Typography>
-                            <Typography variant='body2' color='textSecondary' noWrap className='p-2'>
-                                Description that can be long and should stay in a single line and truncate if too lengthy. Phasellus sed sapien maximus, vestibulum urna ultricies, mattis metus. Etiam pretium cursus quam sit amet hendrerit. Morbi urna enim, fermentum ut vestibulum eget, ultricies ac eros. Sed suscipit porta massa, feugiat feugiat sem feugiat eu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ex ex, eleifend
-                            </Typography>
-                        </Box>
-                        <Button
-                            onClick={() => enterModule(classId) }
-                            sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginLeft: '16px' }}>
-                            <ArrowForwardIosIcon />
-                        </Button>
-                    </div>
+                        </div>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={3}>
-                    <div className='' style={{ borderRadius: '10px' }}>
-                        <Typography variant='body2' color='textSecondary' className='flex'>
-                            <Checkbox isChecked={isInstructionComplete} handleChange={handleCheckboxChange} />
-                            <span className='px-5'>
-                                Instruction Completed
-                            </span>
-                        </Typography>
-                    </div>
-                </Grid>
-            </Grid>
+            ))}
         </>
     );
 }
